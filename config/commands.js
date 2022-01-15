cdfa3ea6350d94b6362628f9cab6d86b5ab125b9
Sajawal Fareedi <sajawalfareedi448@gmail.com>
2022-01-15 17:19:05 +0500

    Added some Commands for Bot

---
+++ config/commands.js
@@ -0,0 +1,102 @@
+module.exports = [
+  {
+    name: "help",
+    description:
+      "See a list of all commands. Additionally, can display more information on most items/commands",
+  },
+  {
+    name: "rules",
+    description:
+      "Displays some of the major Discord ToS rules that players should not violate",
+  },
+  {
+    name: "begin",
+    description:
+      "Displays basic information to help give players an idea of where and how to start",
+  },
+  {
+    name: "profile",
+    description:
+      "Shows your current statistics, use 'profile @player' to see another player's stats",
+  },
+  {
+    name: "p",
+    description:
+      "Shows your current statistics, use 'p @player' to see another player's stats",
+  },
+  {
+    name: "inventory",
+    description: "Shows your inventory",
+  },
+  {
+    name: "i",
+    description: "Shows your inventory",
+  },
+
+  {
+    name: "inv",
+    description: "Shows your inventory",
+  },
+  {
+    name: "bag",
+    description: "Shows your inventory",
+  },
+  {
+    name: "leaderboard",
+    description:
+      "Shows the top players (there are server and global leaderboards)",
+  },
+  {
+    name: "lbr",
+    description:
+      "Shows the top players (there are server and global leaderboards)",
+  },
+  {
+    name: "quest",
+    description: "Shows your current quest if you have one",
+  },
+  {
+    name: "adventure",
+    description: "Used to find mobs to get XP and coins.",
+  },
+  {
+    name: "hunt",
+    description: "Used to find mobs to get XP and coins.",
+  },
+  {
+    name: "search",
+    description: "Used to find mobs to get XP and coins.",
+  },
+  {
+    name: "h",
+    description: "Used to find mobs to get XP and coins.",
+  },
+  {
+    name: "adv",
+    description: "Used to find mobs to get XP and coins.",
+  },
+  {
+    name: "s",
+    description: "Used to find mobs to get XP and coins.",
+  },
+  {
+    name: "battle",
+    description:
+      "Use battle @player to fight a duel with another player for coins and exp.",
+  },
+  {
+    name: "challenge",
+    description:
+      "Use challenge @player to fight a duel with another player for coins and exp.",
+  },
+  {
+    name: "b",
+    description:
+      "Use b @player to fight a duel with another player for coins and exp.",
+  },
+  {
+    name: "ch",
+    description:
+      "Use ch @player to fight a duel with another player for coins and exp.",
+  },
+];
